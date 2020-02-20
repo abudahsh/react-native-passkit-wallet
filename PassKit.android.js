f4e0bdcb6895cc49b0e3887a81b28245f53f3ca1
Mohammed <willcodahsh@gmail.com>
2020-02-20 09:56:51 +0300

    remove android from package

--- PassKit.android.js
+++ PassKit.android.js
@@ -1,29 +1,29 @@
-/**
- * @flow
- */
-'use strict'
+// /**
+//  * @flow
+//  */
+// 'use strict'
 
-import { NativeModules } from 'react-native'
-import type EmitterSubscription from 'EmitterSubscription'
+// import { NativeModules } from 'react-native'
+// import type EmitterSubscription from 'EmitterSubscription'
 
-const nativeModule = NativeModules.RNPassKit
+// const nativeModule = NativeModules.RNPassKit
 
-export default {
-  ...nativeModule,
+// export default {
+//   ...nativeModule,
 
-  presentAddPassesViewController: (base64Encoded: string): Promise<void> => {
-    // eslint-disable-next-line no-console
-    console.warn('PassKit.presentAddPassesViewController is deprecated. Use PassKit.addPass instead.')
-    return nativeModule.addPass(base64Encoded)
-  },
+//   presentAddPassesViewController: (base64Encoded: string): Promise<void> => {
+//     // eslint-disable-next-line no-console
+//     console.warn('PassKit.presentAddPassesViewController is deprecated. Use PassKit.addPass instead.')
+//     return nativeModule.addPass(base64Encoded)
+//   },
 
-  addEventListener: (
-    eventType: string,
-    listener: Function,
-    context: ?Object,
-  ): ?EmitterSubscription => (
-    null
-  ),
+//   addEventListener: (
+//     eventType: string,
+//     listener: Function,
+//     context: ?Object,
+//   ): ?EmitterSubscription => (
+//     null
+//   ),
 
-  removeEventListener: (eventType: string, listener: Function): void => {},
-}
+//   removeEventListener: (eventType: string, listener: Function): void => {},
+// }
